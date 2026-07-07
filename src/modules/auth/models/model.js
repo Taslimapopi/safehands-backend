be12ef7ed566c21eae0fa0d3c5985253a005ac25
taslima <garryanderson337@gmail.com>
2026-07-07 19:21:53 +0600

    method for comparing password ADDed in usedModel

--- src/modules/auth/models/model.js
+++ src/modules/auth/models/model.js
@@ -1,53 +1,58 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-    name : {
-        type: String,
-        required : [true, 'name is required'],
-        trim: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      trim: true,
     },
-    email:{
-        type: String,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        required: [true, 'email is required']
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: [true, "email is required"],
+      match: [/^\S+@\S+\.\S+$/, "enter a valid email"],
     },
-    password : {
-        type: String,
-        minlength: [6, 'password at least 6 characters'],
-        required : [true, 'password is required'],
-        select : false
+    password: {
+      type: String,
+      minlength: [6, "password at least 6 characters"],
+      required: [true, "password is required"],
+      select: false,
     },
     role: {
-        type : String,
-        enum : ['user', 'client','provider','admin'],
-        default: 'user'
+      type: String,
+      enum: ["user", "client", "provider", "admin"],
+      default: "user",
     },
     phone: {
-        type: String,
-        required: [true, 'phone number is required'],
-        trim: true
+      type: String,
+      required: [true, "phone number is required"],
+      trim: true,
     },
-    location : {
-        type: String,
-        trim: true
-    }
+    location: {
+      type: String,
+      trim: true,
+    },
+  },
+  { timestamps: true },
+);
 
-},
-{timestamps: true}
-)
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-userSchema.pre('save', async function (next){
-    if(!this.isModified('password')){
-        return next()
-    }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+userSchema.methods.comparePassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
 
-const User = mongoose.models.User || mongoose.model('User',userSchema)
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
